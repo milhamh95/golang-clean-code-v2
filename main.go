12d3654c5e5dc3173c44dec8ecad8aeaaa616819
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-09-07 02:22:52 +0700

    add print

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	fmt.Println("test")
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello")
