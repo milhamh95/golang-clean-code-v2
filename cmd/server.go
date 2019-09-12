9d4af9a630f2cdd5b4644fc2c500b0e2683dbc7b
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-09-12 22:40:10 +0700

    add simple server

--- cmd/server.go
+++ cmd/server.go
@@ -1,18 +1,30 @@
 package cmd
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+const address = ":8500"
 
 var serverCmd = &cobra.Command{
 	Use:   "http",
 	Short: "Start http server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("++++++++ print ++++++++")
-		fmt.Println("start server")
-		fmt.Println("+++++++++++++++++")
+		e := echo.New()
+
+		e.GET("ping", func(c echo.Context) error {
+			return c.JSON(http.StatusOK, "pong")
+		})
+
+		log.Info("Starting HTTP server at ", address)
+		err := e.Start(address)
+		if err != nil {
+			log.Fatalf("Failed to start server: %s", err.Error())
+		}
 	},
 }
 
