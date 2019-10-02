c1aec2032cc742e55d73017551732d03c6708491
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-10-02 21:09:26 +0700

    feat(domain): add department

--- domain/employee.go
+++ domain/employee.go
@@ -1,9 +1,25 @@
-package employee
+package domain
 
+import "time"
+
+//EmployeeFilter reqpresent query filter
 type EmployeeFilter struct {
 	IDs     []string
 	Keyword string
 	Num     int
 	Cursor  string
 	DeptIDs []string
+}
+
+// Employee represent employee data
+type Employee struct {
+	ID          string    `json:"id"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	BirthPlace  string    `json:"birth_place"`
+	DateOfBirth time.Time `json:"date_of_birth"`
+	Title       string    `json:"title"`
+	Department  Department
+	CreatedTime time.Time `json:"created_time"`
+	UpdatedTime time.Time `json:"updated_time"`
 }
