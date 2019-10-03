26b1a735d6cd971156a83786d7f436cfd4fe5578
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-10-03 21:10:53 +0700

    feat(repo): implement dummy function employee

--- employee/repository/mariadb/employee.go
+++ employee/repository/mariadb/employee.go
@@ -1,7 +1,10 @@
 package mariadb
 
 import (
+	"context"
 	"database/sql"
+
+	"github.com/milhamhidayat/golang-clean-code-v2/domain"
 )
 
 // Repository implement all employee repository method from interface
@@ -14,4 +17,29 @@ func New(db *sql.DB) Repository {
 	return Repository{
 		DB: db,
 	}
+}
+
+// Create is a repository to insert an employee
+func (r Repository) Create(ctx context.Context, e *domain.Employee) (err error) {
+	return nil
+}
+
+// Get is a repository to get an employee
+func (r Repository) Get(ctx context.Context, employeeID string) (employee domain.Employee, err error) {
+	return
+}
+
+// Fetch is a repository to fetch employees
+func (r Repository) Fetch(ctx context.Context, filter domain.EmployeeFilter) (employees []domain.Employee, nextCursor string, err error) {
+	return
+}
+
+// Update is a repository to update an employee
+func (r Repository) Update(ctx context.Context, e domain.Employee) (employee domain.Employee, err error) {
+	return
+}
+
+// Delete is a repository to delete an employee
+func (r Repository) Delete(ctx context.Context, employeeID string) (err error) {
+	return
 }
